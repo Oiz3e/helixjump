3d06b1dc949af78c9826a8abfc02aae1cfd58aa2
Izdihar Dhawy Tasdid Tasdid <129589195+Oiz3e@users.noreply.github.com>
2024-12-29 21:26:23 +0700

    ft: from local

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public float bounceForce = 400f;
+
+    public GameObject splitPrefap;
+
+    private void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter (Collision other) {
+        rb.velocity = new Vector3 (rb.velocity.x, bounceForce * Time.deltaTime, rb.velocity.z);
+
+        GameObject newsplit = Instantiate (splitPrefap, new Vector3 (transform.position.x, other.transform.position.y + 0.19f, transform.position.z), transform.rotation);
+        newsplit.transform.localScale = Vector3.one * Random.Range (0.7f, 1.3f);
+        newsplit.transform.parent = other.transform;
+
+        string materialName = other.transform.GetComponent<MeshRenderer> ().material.name;
+        Debug.Log (materialName);
+
+        if(materialName == "Safe (Instance)") {
+            Debug.Log ("you are safe");
+        }
+
+        if(materialName == "UnSafe (Instance)") {
+            GameManager.gameOver = true;
+        }
+
+        if(materialName == "LastRing (Instance)") {
+            GameManager.levelWin = true;
+        }
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour { 
+    public static bool gameOver; public static bool levelWin;
+
+    public GameObject gameOverPanel;
+    public GameObject levelWinPanel;
+
+    private void Start() {
+        Time.timeScale = 1f;
+        gameOver = false;
+        levelWin = false;
+    }
+
+    private void Update () {
+        if (gameOver) {
+            Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+            if(Input.GetMouseButtonDown(0)) {
+                SceneManager.LoadScene(0);
+            }
+        }
+
+        if(levelWin) {
+            levelWinPanel.SetActive(true);
+            if(Input.GetMouseButtonDown(0)) {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
